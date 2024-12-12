2f169d9203ebc5f2e21ac5eaf325290aa33a33f8
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-12-12 15:27:42 +0300

    cheesboard.cpp created.

---
+++ src/ChessBoard.cpp
@@ -0,0 +1,48 @@
+#include "ChessBoard.hpp"
+
+ChessBoard::ChessBoard(int size) : boardSize(size){
+    //create board
+}
+
+void ChessBoard::createBoard(const std::vector<PieceConfig>& piece, const std::vector<PortalConfig>& portal){
+
+}
+
+void ChessBoard::printBoard() const{
+
+}
+
+void ChessBoard::movePiece(int beforex, int beforey, int afterx, int aftery){
+    auto moves = possibleMoves(beforex, beforey);
+    for(auto& move : moves){
+        if(move.first == afterx && move.second == aftery){
+            //change board
+            break;
+        }
+    }
+}
+
+std::vector<std::pair<int,int>> ChessBoard::possibleMoves(int x, int y){
+    std::vector<std::pair<int,int>> moves;
+    for(int i = 0; i < boardSize; ++i){
+        for(int j = 0; j < boardSize; ++j){
+            if(isValidMove(x, y, i, j)){
+                //highlight there
+                moves.push_back(i,j);
+            }
+        }
+    }
+    return moves;
+}
+
+ChessPiece* ChessBoard::getPiece(int x, int y) const{
+
+}
+
+Portal* ChessBoard::getPortal(int x, int y) const{
+    
+}
+
+bool ChessBoard::isValidMove(int beforex, int beforey, int afterx, int aftery) const{
+
+}
