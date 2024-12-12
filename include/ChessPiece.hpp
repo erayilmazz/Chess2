086a94312190af9ffaf1e5b725499f4c0ca723d9
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-12-12 12:52:56 +0300

    ChessPiece.hpp created.

---
+++ include/ChessPiece.hpp
@@ -0,0 +1,25 @@
+#include <string>
+
+
+
+struct Position{
+    int x;
+    int y;
+};
+
+class ChessPiece{
+public:
+    ChessPiece(const std::string& type, const Position& position, const std::string& color);
+
+    std::string getType() const;
+    Position getPosition() const;
+    std::string getColor() const;
+
+    void setPosition(const Position& position);
+    void setType(const std::string& newType);
+
+private:
+    std::string type;
+    Position position;
+    std::string color;
+};
