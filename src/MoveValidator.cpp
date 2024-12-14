194560134e03440f4c2dd196c7d3e87306196df3
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-12-14 13:51:48 +0300

    validmoves vector added.

--- src/MoveValidator.cpp
+++ src/MoveValidator.cpp
@@ -5,23 +5,33 @@ MoveValidator::MoveValidator(const Board& board)
 
 
 std::vector<Position> MoveValidator::calculatePossibleMoves(const ChessPiece& piece){
-    if(piece.cooldown != 0) return; //empty vector
+    std::vector <Position> cordinates;
+    if(piece.cooldown != 0) return cordinates; //empty vector
     MovementRules movement = piece.getRules();
+    Position currPos = piece.getPosition();
     for(int i = 0; i < movement.forward; ++i){
-        Position tempPos = piece.getPosition();
+        Position tempPos = currPos;
         tempPos.y += 1;
-        if(isValidMove(piece, tempPos))
+        if(isValidMove(piece, tempPos)){
+            cordinates.push_back(tempPos);
+        }else{
+            break;
+        }
     }
     for(int i = 0; i < movement.backward; ++i){
-        Position tempPos = piece.getPosition();
+        Position tempPos = currPos;
         tempPos.y -= 1;
-        if(isValidMove(piece, tempPos))
+        if(isValidMove(piece, tempPos)){
+            cordinates.push_back(tempPos);
+        }else{
+            break;
+        }
     }
     if(movement.sideways != 1){
         for(int i = 0; i < movement.sideways; ++i){
-            Position tempPos = piece.getPosition();
+            Position tempPos = currPos;
             tempPos.x +=1;
-            if(isValidMove(piece, tempPos)) 
+            if(isValidMove(piece, tempPos))
         }
         for(int i = 0; i < movement.sideways; ++i){
             Position tempPos = piece.getPosition();
@@ -34,25 +44,25 @@ std::vector<Position> MoveValidator::calculatePossibleMoves(const ChessPiece& pi
             Position tempPos = piece.getPosition();
             tempPos.x +=1;
             tempPos.y +=1;
-            if(isValidMove(piece, tempPos))
+            if(!isValidMove(piece, tempPos)) break;
         }
         for(int i = 0; i < movement.diagonal; ++ i){
             Position tempPos = piece.getPosition();
             tempPos.x +=1;
             tempPos.y -=1;
-            if(isValidMove(piece, tempPos))
+            if(!isValidMove(piece, tempPos)) break;
         }
         for(int i = 0; i < movement.diagonal; ++ i){
             Position tempPos = piece.getPosition();
             tempPos.x -=1;
             tempPos.y +=1;
-            if(isValidMove(piece, tempPos))
+            if(!isValidMove(piece, tempPos)) break;
         } 
         for(int i = 0; i < movement.diagonal; ++ i){
             Position tempPos = piece.getPosition();
             tempPos.x -=1;
             tempPos.y -=1;
-            if(isValidMove(piece, tempPos))
+            if(!isValidMove(piece, tempPos)) break;
         }  
     }
     if(movement.l_shape){
@@ -75,15 +85,21 @@ std::vector<Position> MoveValidator::calculatePossibleMoves(const ChessPiece& pi
             {tempPos.x -= 2, tempPos.y +=4},
             {tempPos.x -= 2, tempPos.y -=4}
         }
+        for(const auto& move : lMoves){
+            isValidMove(piece, move);
+        }
     }
-
-
 }
 
 
 
 bool MoveValidator::isValidMove(const ChessPiece& piece, Position& cor) const{
     if(piece.cooldown || afterx >= board.getSize() || aftery >= board.getSize() || afterx < 0 || aftery < 0) return false;
-    
+    if(board.getPiece(cor) != nullptr){
+        //highlight et burda
+        return true;
+    }
+    return false;
 
 }
+
